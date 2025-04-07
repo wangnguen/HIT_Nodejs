34775604f2e42b82dc69cd009f6e3a6e79bfd133
wangnguen <160375039+wangnguen@users.noreply.github.com>
2025-04-07 16:58:37 +0700

    Buoi5 (#4)
    * feat: upload buoi5
    
    * Btvn buoi5/wang (#1)
    
    * feat: implement get user by id
    
    * feat: implement delete user by id
    
    * refactor: get user by id
    
    * feat: implement search query by name
    
    * fix: connect db
    
    * Btvn buoi5/wang (#3)
    
    * feat: implement get user by id
    
    * feat: implement delete user by id
    
    * refactor: get user by id
    
    * feat: implement search query by name
    
    * fix: connect db
    
    * feat: User authentication feature

--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 
 const userController = require('../../controllers/client/user.controller');
 
+const userValidate = require('../../validates/client/account.validate');
+
 // query > param
 router.get('/', userController.getUsers);
 router.get('/search', userController.searchUserByName);
 router.get('/:id', userController.getUserById);
 
-router.post('/', userController.createUser);
+router.post('/', userValidate.userCreatePost, userController.createUser);
 
 router.put('/:id', userController.updateUser);
 
