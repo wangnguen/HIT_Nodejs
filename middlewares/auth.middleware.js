e91b58d2652c669897370887eeb23323b4de04a1
Quang Nguyen <wangnguenlc79@gmail.com>
2025-04-15 00:32:22 +0700

    fix: authen middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,31 +1,32 @@
 const jwt = require('jsonwebtoken');
+const httpStatus = require('http-status-codes');
 const User = require('../models/user.model');
+const ApiError = require('../utils/ApiError');
 
 const verifyToken = async (req, res, next) => {
   try {
     const token = req.cookies.token;
+
     if (!token) {
-      res.redirect(`/localhost:3000/auth/login`);
-      return;
+      throw new ApiError(httpStatus.UNAUTHORIZED, 'Vui lòng đăng nhập!');
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const { id, email } = decoded;
 
-    const existAccount = await AccountAdmin.findOne({
-      _id: id,
-      email: email,
-    });
-
-    if (!existAccount) {
-      res.clearCookie('token');
-      res.redirect(`/localhost:3000/account/login`);
-      return;
+    const user = await User.findOne({ _id: id, email });
+    if (!user) {
+      throw new ApiError(httpStatus.UNAUTHORIZED, 'Người dùng không tồn tại');
     }
+
     next();
   } catch (error) {
     res.clearCookie('token');
-    res.redirect(`/localhost:3000/account/login`);
+    res.status(httpStatus.UNAUTHORIZED).json({
+      httpStatus: httpStatus.UNAUTHORIZED,
+      message: 'Token không hợp lệ hoặc đã hết hạn',
+    });
   }
 };
+
 module.exports = { verifyToken };
